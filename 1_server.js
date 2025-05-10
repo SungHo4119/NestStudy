7116cbbfbd42e0f49f0f2a8ff0c58f80f2ecd9bc
임성호 <tjd4119@naver.com>
2025-05-10 16:01:30 +0900

    2일차 - Path 별로 다른 응답 변환하기 ( 재밋음 )

--- 1_server.js
+++ 1_server.js
@@ -1,11 +1,27 @@
 import http from 'http';
+import url from 'url';
 
 const host = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Context-Type': 'text/html' });
-  res.end('<h1> Hello World</h1>');
+  // req.url  = 요청이 들어온 url
+  const path = url.parse(req.url).path;
+
+  if (path === '/') {
+    // http://localhost:3000 인 경우 '/' 와 같다
+    res.writeHead(200, { 'Context-Type': 'text/html' });
+    res.end('<h1> Home Page</h1>');
+  } else if (path === '/post') {
+    res.writeHead(200, { 'Context-Type': 'text/html' });
+    res.end('<h1> Post Page</h1>');
+  } else if (path === '/user') {
+    res.writeHead(200, { 'Context-Type': 'text/html' });
+    res.end('<h1> User Page</h1>');
+  } else {
+    res.writeHead(404, { 'Context-Type': 'text/html' });
+    res.end('<h1> 404 Page Not Found</h1>');
+  }
 });
 
 server.listen(port, host, () => {
