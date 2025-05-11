75262d301414105040e8ded046b4074ee6ceebb2
임성호 <tjd4119@naver.com>
2025-05-11 18:04:25 +0900

    3일차 - Service로 로직 옮기기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,56 +3,14 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
+import { PostsModule } from 'src/posts/posts.module';
 import { PostsService } from './posts.service';
 
 // nest g resource 를 이용해서 모듈을 생성 할 수 있다.
-/**
- * author: string;
- * title: string;
- * content: string;
- * likeCount: number;
- * commentCount: number;
- */
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newJeans_Official',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지',
-    likeCount: 100000,
-    commentCount: 5000000,
-  },
-  {
-    id: 2,
-    author: 'newJeans_Official',
-    title: '뉴진스 해린',
-    content: '노래 연습 하고 있는 해린',
-    likeCount: 9999,
-    commentCount: 49999,
-  },
-  {
-    id: 3,
-    author: 'newJeans_Official',
-    title: '뉴진스 로제',
-    content: '운동중인 로제',
-    likeCount: 8888,
-    commentCount: 399999,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
@@ -63,8 +21,8 @@ export class PostsController {
    * 모든 포스트를 가져오는 API
    */
   @Get('')
-  getPosts(): PostModel[] {
-    return posts;
+  getPosts(): PostsModule[] {
+    return this.postsService.getAllPosts();
   }
 
   /**
@@ -72,14 +30,8 @@ export class PostsController {
    * 특정 포스트를 가져오는 API
    */
   @Get(':id')
-  getPost(@Param('id') id: number) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      // NotFoundException - NestJS에서 기본으로 제공하는 에러 타입
-      throw new NotFoundException();
-    }
-
-    return post;
+  getPost(@Param('id') id: number): PostsModule {
+    return this.postsService.getPostById(+id);
   }
   /**
    * Post /posts
@@ -90,17 +42,8 @@ export class PostsController {
     @Body('author') author: string,
     @Body('title') title: string,
     @Body('content') content: string,
-  ) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author: author,
-      title: title,
-      content: content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-    posts = [...posts, post];
-    return post;
+  ): PostsModule {
+    return this.postsService.createPost(author, title, content);
   }
 
   /**
@@ -113,27 +56,8 @@ export class PostsController {
     @Body('author') author?: string,
     @Body('title') title?: string,
     @Body('content') content?: string,
-  ) {
-    const post: PostModel | undefined = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    if (author) {
-      post.author = author;
-    }
-    if (title) {
-      post.title = title;
-    }
-    if (content) {
-      post.content = content;
-    }
-
-    posts = posts.map((prvePost: PostModel) =>
-      prvePost.id === +id ? post : prvePost,
-    );
-
-    return post;
+  ): PostsModule {
+    return this.postsService.updatePost(+id, author, title, content);
   }
 
   /**
@@ -142,14 +66,7 @@ export class PostsController {
    */
 
   @Delete(':id')
-  deletePost(@Param('id') id: string) {
-    const post: PostModel | undefined = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    posts = posts.filter((post) => post.id !== +id);
-
-    return id;
+  deletePost(@Param('id') id: string): number {
+    return this.postsService.deletePost(+id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,108 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newJeans_Official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 100000,
+    commentCount: 5000000,
+  },
+  {
+    id: 2,
+    author: 'newJeans_Official',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 해린',
+    likeCount: 9999,
+    commentCount: 49999,
+  },
+  {
+    id: 3,
+    author: 'newJeans_Official',
+    title: '뉴진스 로제',
+    content: '운동중인 로제',
+    likeCount: 8888,
+    commentCount: 399999,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts(): PostModel[] {
+    return posts;
+  }
+
+  getPostById(id: number): PostModel {
+    const post = posts.find((post) => post.id === id);
+    if (!post) {
+      // NotFoundException - NestJS에서 기본으로 제공하는 에러 타입
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author: author,
+      title: title,
+      content: content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+    posts = [...posts, post];
+    return post;
+  }
+
+  updatePost(id: number, author?: string, title?: string, content?: string) {
+    const post: PostModel | undefined = posts.find((post) => post.id === id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+    if (title) {
+      post.title = title;
+    }
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prvePost: PostModel) =>
+      prvePost.id === id ? post : prvePost,
+    );
+
+    return post;
+  }
+
+  deletePost(id: number) {
+    const post: PostModel | undefined = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    posts = posts.filter((post) => post.id !== +id);
+
+    return id;
+  }
+}
